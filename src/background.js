f9ac846b61705bad78c1cfa4ababd0f3f0cec887
pdrojack <singhpd75@gmail.com>
2022-03-03 02:08:58 +0530

    version 2

--- src/background.js
+++ src/background.js
@@ -1,149 +1,183 @@
-// Creates random token for user id
-function getRandomToken() {
-    // E.g. 8 * 32 = 256 bits token
-    var randomPool = new Uint8Array(32);
-    crypto.getRandomValues(randomPool);
-    var hex = '';
-    for (var i = 0; i < randomPool.length; ++i) {
-        hex += randomPool[i].toString(16);
-    }
-    // E.g. db18458e2782b2b77e36769c569e263a53885a9944dd0a861e5064eac16f1a
-    return hex;
-}
+let local_cache = {
+  menu: []
+};
 
-const api = "https://api.injext.tech/api"
+const api = "https://api.github.com";
 
-local_cache = {}
-
-// First level , add directly to parents 
+// First level , add directly to parents
 const parentMenu = chrome.contextMenus.create({
   title: "injext",
   id: "injext",
-  contexts: ["editable"]
-})
+  contexts: ["editable"],
+});
 
-chrome.storage.sync.get(['userId']).then(res => {
+const getTemplateNames = async (username, repo) => {
 
-  local_cache.userId = res.userId;
+  var event = new Event('menu')
 
-  const templates = getTemplateNames(res.userId);
-
-  templates.then( body => {
-
-    body.data.map( t => {
-
-      if(t.key ==="#") {
-        
-        // First level , add directly to parents 
-        t.value.map( c => {
-          
-          chrome.contextMenus.create({
-            title: c.split(".")[0],
-            id: c,
-            parentId: "injext",
-            contexts: ["editable"]
-          })
-          
-        })
-        
-      } else {
-        
-        // Second Level directories, add directories to parent then children to these directories
-        chrome.contextMenus.create({
-          title: t.key,
-          id: t.key,
-          parentId: "injext",
-          contexts: ["editable"]
-        })
-        
-        t.value.map( c => {
-          
-          chrome.contextMenus.create({
-            title: c.split(".")[0],
-            id: c,
-            parentId: t.key,
-            contexts: ["editable"]
-          })
-          
-        })
-      }
-
-    })
-
-  })
-
-})
-
-const getTemplateNames = async ( userId ) => {
-
-  const response  = await fetch(`${api}/templates/${userId}`, {
-      method: "GET",
-      mode: "no-cors",
-      headers: {
-          'Content-Type': "application/json"
-      }
-  })
-
-  return response.json();
-
-}
-
-const getTemplateData = async ( userId, templateName ) => {
-  
-  templateName = JSON.stringify(templateName)
-
-  const response  = await fetch(`${api}/servetemplate/${userId}/${templateName}`, {
-      method: "GET",
-      mode: "no-cors",
-      headers: {
-          'Content-Type': "application/json"
-      }
-  })
-  
-  return response.json();
-
-} 
-
-const clickSelection = function (info, selectedTab) {
-
-  chrome.tabs.query({active: true, currentWindow: true}, function() {
-
-      getTemplateData( local_cache.userId , [info.parentMenuItemId, info.menuItemId]).then( res => {
-          const body = res.template;
-          chrome.tabs.sendMessage(selectedTab.id, { data: body }, function(response) {
-              console.log("success")
-          });
-      })
-
+  const response = await fetch(api + `/repos/${username}/${repo}/contents`, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
 
-}
+  const data = await response.json();
+
+  data.forEach( async (i) => {
+    if (i.type === "file") {
+      local_cache.menu.push({ key: "#", value: [i.name]})
+      dispatchEvent(event);
+    } else {
+      const response2 = await fetch(
+        api + `/repos/${username}/${repo}/contents/${i.name}`,
+        {
+          method: "GET",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const data2 = await response2.json();
+
+      const data2Arr = [];
+
+      data2.forEach((j) => {
+        if (j.type == "file") {
+          data2Arr.push(j.name);
+        }
+      });
+
+      local_cache.menu.push({ key: i.name, value: data2Arr });
+
+    }
+    dispatchEvent(event);
+  });
+};
+
+
+addEventListener("menu", function prepare(){
+  local_cache.menu.map((t) => {
+    if (t.key === "#") {
+      // First level , add directly to parents
+      t.value.map((c) => {
+        chrome.contextMenus.create({
+          title: c.split(".")[0],
+          id: c,
+          parentId: "injext",
+          contexts: ["editable"],
+        });
+      });
+    } else {
+      // Second Level directories, add directories to parent then children to these directories
+      chrome.contextMenus.create({
+        title: t.key,
+        id: t.key,
+        parentId: "injext",
+        contexts: ["editable"],
+      });
+
+      t.value.map((c) => {
+        chrome.contextMenus.create({
+          title: c.split(".")[0],
+          id: c,
+          parentId: t.key,
+          contexts: ["editable"],
+        });
+      });
+    }
+  });
+})
+
+chrome.storage.sync
+  .get(["repo"])
+  .then(async (res) => {
+    local_cache.username = res.repo.split("/").at(-2);
+    local_cache.repoName = res.repo.split("/").at(-1).split(".").at(0);
+
+      const templates = getTemplateNames(
+        local_cache.username,
+        local_cache.repoName
+      )
+
+})
+
+const getTemplateData = async (username, repo, path) => {
+
+  if(path[0] == "injext"){
+    path = path[1]
+  } else {
+    path = path[0] + "/" + path[1]
+  }
+  
+  const response = await fetch(
+    api + `/repos/${username}/${repo}/contents/${path}`,
+    {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const d = await response.json();
+
+  return atob(d.content.replace("\n",""));
+};
+
+const clickSelection = function (info, selectedTab) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function () {
+    getTemplateData(local_cache.username, local_cache.repoName, [
+      info.parentMenuItemId,
+      info.menuItemId,
+    ]).then((res) => {
+      const body = res.template;
+      chrome.tabs.sendMessage(
+        selectedTab.id,
+        { data: body },
+        function (response) {
+          console.log("success");
+        }
+      );
+    });
+  });
+};
 
 chrome.contextMenus.onClicked.addListener(clickSelection);
 
 // Setup UUID for user
-chrome.runtime.onInstalled.addListener(async function(){
+chrome.runtime.onInstalled.addListener(async function () {
+  function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+      (
+        c ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+      ).toString(16)
+    );
+  }
 
-    function uuidv4() {
-      return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-      );
-    }
-    
-    let newUserId = await uuidv4()
+  let newUserId = await uuidv4();
 
-    // Check if userId in DB
-    chrome.storage.sync.get(['userId']).then( res => {
-        if (res === null || res.userId === null){
-          // Generate Id
-          chrome.storage.sync.set({
-            "userId": newUserId
-          }, function(){
+  // Check if userId in DB
+  chrome.storage.sync
+    .get(["userId"])
+    .then((res) => {
+      if (res === null || res.userId === null) {
+        // Generate Id
+        chrome.storage.sync.set(
+          {
+            userId: newUserId,
+          },
+          function () {
             console.log("new User Id: " + newUserId);
-          })
-        }
-    }).catch( err => {
-      console.log(err)
+          }
+        );
+      }
     })
-
+    .catch((err) => {
+      console.log(err);
+    });
 });
--- src/popup.js
+++ src/popup.js
@@ -4,7 +4,7 @@ let inputArea = document.getElementById("git-input");
 let inputButton = document.getElementById("input-button");
 let changeRepo = document.getElementById("change-repo");
 
-// https://github.com/PDROJACK/rpc_fileserver.git
+const API = "https://api.github.com";
 
 chrome.storage.sync.get("repo", function ({ repo }) {
   // Display the git input or change input repo here
@@ -23,31 +23,36 @@ changeRepo.addEventListener("click", function showChangeRepo() {
   changeRepo.style.display = "none";
 });
 
-inputButton.addEventListener("click", function sendRepo() {
-  let val = document.getElementById("text-input").value;
+const clickkk = function sendRepo() {
 
-  if (val.split(".").pop() === "git") {
-    // Send Request and set response here
-    chrome.storage.sync.get("userId", ({ userId }) => {
-      fetch("http://localhost:5000/link", {
-        method: "POST",
-        mode: "no-cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          userId,
-          repo: val,
-        }),
-      }).then((response) => {
-        chrome.storage.sync.set({ repo: val }, ()=>{
-            inputArea.style.display = "none";
-            changeRepo.style.display = "inline";
-        });
-      });
-    });
-  } else {
-    // Invalid url please enter again
-    alert("Invalid URL please enter again");
-  }
-});
+  let repo = document.getElementById("text-input").value;
+
+  if (repo.split(".").pop() === "git") {
+
+    const data = repo.split("/")
+    const user = data.at(-2)
+    const repoName = data.at(-1).split(".").at(0)
+
+    fetch(API+`/repos/${user}/${repoName}`, {
+          method: "GET",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json"
+          }}
+        ).then(async (response) => {
+          const res = await response.json();
+          if(!res.private){
+            chrome.storage.sync.set({ repo }, ()=>{
+              console.log("linkeds")
+              inputArea.style.display = "none";
+              changeRepo.style.display = "inline";
+            })
+          }
+      }).catch(err => console.log(err));
+    } else {
+      alert("Invalid URL please enter again");
+    }
+
+}
+
+inputButton.addEventListener("click", clickkk);
